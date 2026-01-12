792059bb81020bef7e73e604e9e104ba03abe5bd
BrHiba <elberkaoui208@gmail.com>
2026-01-12 14:57:14 +0100

    fix case sensitive imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Services from "../sections/Services";
 import Work from "../sections/Work";
 import Partners from "../sections/Partners";
 import Footer from "../sections/Footer";
-import Hero from "../sections/hero";
+import Hero from "../sections/Hero";
 import About from "../sections/About";
 
 function Home() {
